c7ce0be1a78000f367e7bdfbfb168174de505595
melissaturbano <melissaturbano@gmail.com>
2022-05-17 17:19:47 -0300

    refactor: add database setup class

--- Program.cs
+++ Program.cs
@@ -1,21 +1,7 @@
 ﻿using Microsoft.Data.Sqlite;
+using LabManager.Database;
 
-var connection = new SqliteConnection("Data Source=database.db");
-connection.Open();
-
-var command = connection.CreateCommand();
-command.CommandText = @"
-        CREATE TABLE IF NOT EXISTS Computers(
-                id int not null primary key,
-                ram varchar(100) not null,
-                processador varchar(100) not null
-        );
-";
-
-command.ExecuteNonQuery();
-
-connection.Close(); 
-
+new DatabaseSetup();
 
 
 //Routing 
@@ -27,10 +13,10 @@ if(modelName == "Computer")
         if(modelAction == "List")
         {
                 Console.WriteLine("Computer List");
-                connection = new SqliteConnection("Data Source=database.db");
+                var connection = new SqliteConnection("Data Source=database.db");
                 connection.Open();
 
-                command = connection.CreateCommand();
+                var command = connection.CreateCommand();
                 command.CommandText = "SELECT * FROM Computers;";
 
                 var reader = command.ExecuteReader();
@@ -50,10 +36,10 @@ if(modelName == "Computer")
                 var ram = args[3];
                 var processor = args[4];
 
-                connection = new SqliteConnection("Data Source=database.db");
+                var connection = new SqliteConnection("Data Source=database.db");
                 connection.Open();
 
-                command = connection.CreateCommand();
+                var command = connection.CreateCommand();
                 command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor);";
                 command.Parameters.AddWithValue("$id", id);
                 command.Parameters.AddWithValue("$ram", ram);
